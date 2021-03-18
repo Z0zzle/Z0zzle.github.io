acf7449b5ccae4bb654b924c42257c44764cb3f2
Z0zzle <80816749+Z0zzle@users.noreply.github.com>
2021-03-18 18:56:22 +0100

    upd

--- node.js
+++
@@ -1,9 +0,0 @@
-
-
-/**
- * Init Alpha Vantage with your API key.
- *
- * @param {String} key 
- *   Your Alpha Vantage API key.
- */
-const alpha = require('alphavantage')({ key: '2PZ84LGO6SC51I67' });
--- node_modules/alphavantage/__mocks__/cross-fetch.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-export default (url) =>
-  Promise.resolve({
-    body: `{}`,
-    status: parseInt(url.slice(-3)),
-    text: () => `{}`,
-    json: () => ({})
-  });
