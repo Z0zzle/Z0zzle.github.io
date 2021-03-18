970dc69d5199eed041aea822fc259acd40106cf4
Z0zzle <80816749+Z0zzle@users.noreply.github.com>
2021-03-18 10:42:33 +0100

    upd

--- node.js
+++ node.js
@@ -6,4 +6,4 @@
  * @param {String} key 
  *   Your Alpha Vantage API key.
  */
-const alpha = require('alphavantage')({ key: 'JRIFSK7RVUAFMRM8' });
+const alpha = require('alphavantage')({ key: '2PZ84LGO6SC51I67' });
